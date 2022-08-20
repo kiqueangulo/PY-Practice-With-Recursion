e26b62ebdf68aebca5125438ccb85dff83b2f71b
kiqueangulo <kiqueangulo@gmail.com>
2022-08-20 11:59:27 -0400

    Complete function 1

--- solution1.py
+++ solution1.py
@@ -1,1 +1,12 @@
 # Write code for algorithm 1 below
+def print_to_zero(number):
+    if number == 0:
+        print(0)
+    elif number < 0:
+        print(number)
+        print_to_zero(number + 1)
+    else:
+        print(number)
+        print_to_zero(number - 1)
+
+print_to_zero(5)
