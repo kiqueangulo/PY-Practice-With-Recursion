0cd3ae97c95e6551964b651bc9d8d4f52e5de026
kiqueangulo <kiqueangulo@gmail.com>
2022-08-20 12:13:14 -0400

    Complete solution 4

--- solution4.py
+++ solution4.py
@@ -1,1 +1,12 @@
 # Write code for algorithm 4 below
+def power(a, b):
+    return a ** b
+
+def recursive_power(a, b):
+    if b == 0:
+        return 1
+    else:
+        return a * recursive_power(a, b - 1)
+
+print(power(2, 4))
+print(recursive_power(2, 4))
