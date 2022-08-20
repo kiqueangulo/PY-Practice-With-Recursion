3b6a36ea72a12bb70fbb7a63cbcc6bdc80c2eb3f
kiqueangulo <kiqueangulo@gmail.com>
2022-08-20 13:30:18 -0400

    Add tribonacci

--- solution3.py
+++ solution3.py
@@ -1,8 +1,18 @@
 # Write code for algorithm 3 below
-def fibonacci_element(n):
+def fibonacci(n):
     if n <= 1:
         return n
     else:
-        return fibonacci_element(n - 1) + fibonacci_element(n - 2)
-        
-print(fibonacci_element(6))
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+def tribonacci(n):
+    if n == 2:
+        return 1
+    elif n <= 1:
+        return 0
+    else:
+        return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3)
+
+
+print(fibonacci(6))
+print(tribonacci(5))
