f6595099f72c6d65d95b6c97271e60bff311b626
kiqueangulo <kiqueangulo@gmail.com>
2022-08-20 12:03:14 -0400

    Complete solution 3
    Fix a typo in solution 2 too

--- solution2.py
+++ solution2.py
@@ -1,5 +1,5 @@
 # Write code for algorithm 2 belowdef count_to(number, start_number = 1):
-def count_to(number, start_number = 1)
+def count_to(number, start_number = 1):
     if start_number <= number:
         print(start_number)
         count_to(number, start_number + 1)
--- solution3.py
+++ solution3.py
@@ -1,1 +1,8 @@
 # Write code for algorithm 3 below
+def fibonacci_element(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci_element(n - 1) + fibonacci_element(n - 2)
+        
+print(fibonacci_element(6))
