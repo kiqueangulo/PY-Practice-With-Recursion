9c38cda46375f477b2a504accbd81b45108ffb26
kiqueangulo <kiqueangulo@gmail.com>
2022-08-20 12:01:35 -0400

    Complete solution 2

--- solution2.py
+++ solution2.py
@@ -1,1 +1,7 @@
-# Write code for algorithm 2 below
+# Write code for algorithm 2 belowdef count_to(number, start_number = 1):
+def count_to(number, start_number = 1)
+    if start_number <= number:
+        print(start_number)
+        count_to(number, start_number + 1)
+
+count_to(5)
